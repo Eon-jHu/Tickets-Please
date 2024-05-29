b86b36cb023b37af13caa1b6ca56201ea6a92dc2
RyanBride22 <141291892+RyanBride22@users.noreply.github.com>
2024-05-29 15:22:26 +1200

    MAIN MENU BUSTING :3
    swagman85

---
+++ Assets/CONTROLLA.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+//using System.Runtime.Hosting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CONTROLLA : MonoBehaviour
+{
+    public CanvasGroup OptionPanel;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void Option()
+    {
+        OptionPanel.alpha = 1;
+        OptionPanel.blocksRaycasts = true;
+    }
+
+    public void Back()
+    {
+        OptionPanel.alpha = 0;
+        OptionPanel.blocksRaycasts = false;
+    }
+
+    public void QuitGame()
+    {
+      Application.Quit();
+    }
+
+}
+
