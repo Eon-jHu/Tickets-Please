21b5eca37e2ea28864177240d668babf422afcba
Lisa Patricia Kelly <lisa.kelly1@MDS.ac.nz>
2024-06-02 21:13:15 +1200

    Added door movement, and sound
    + Added collision around train (cannot exit)
    + Walk through door, no interaction needed

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -7,14 +7,26 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public float FollowSpeed = 2.0f;
+    // --------- Public class variables --------- //
+    public float FollowSpeed = 3.0f;
     public float yOffset = 1.0f;
+    public float xOffset = 0.0f;
     public Transform target;
 
-    // Update is called once per frame
-    void Update()
+    // Variables for clamping camera.
+    public Vector2 MaxPosition;
+    public Vector2 MinPosition;
+
+    // --------- Functions --------- //
+
+    void FixedUpdate()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10.0f);
-        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+        Vector3 newPos = new Vector3(target.position.x - xOffset, target.position.y + yOffset, -10.0f);
+
+        // Clamp camera to minimum and maximum for room size (ensures camera stays within room bounds).
+        newPos.x = Mathf.Clamp(newPos.x, MinPosition.x, MaxPosition.x); // Clamping x position.
+        newPos.y = Mathf.Clamp(newPos.y, MinPosition.y, MaxPosition.y); // Clamping y position.
+
+        transform.position = Vector3.Lerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/RoomTransfer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTransfer : MonoBehaviour
+{
+    // ------------- Public class variables ------------- //
+
+    public Vector2 CameraChangeMin;
+    public Vector2 CameraChangeMax;
+    public Vector3 PlayerChange;
+
+    // ------------- Private class variables ------------- //
+
+    private CameraFollow Cam;
+
+    // ------------- Functions ------------- //
+    private void Start()
+    {
+        // Reference to camera from camera follow class.
+        Cam = Camera.main.GetComponent<CameraFollow>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D _Collision)
+    {
+        // Checking for collision with room switching bounds & player.
+        if (_Collision.CompareTag("Player"))
+        {
+            // Create the new bounds of the room.
+            Cam.MinPosition = CameraChangeMin;
+            Cam.MaxPosition = CameraChangeMax;
+
+            // Moving the player to adjust for new room.
+            _Collision.transform.position += PlayerChange;
+        }
+    }
+}
