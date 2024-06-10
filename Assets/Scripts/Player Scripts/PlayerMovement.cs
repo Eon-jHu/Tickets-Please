6da31a935c04b574c8625fd7e58a8c790d002957
Lisa Patricia Kelly <lisa.kelly1@MDS.ac.nz>
2024-06-10 21:55:44 +1200

    Player Animations
    + Added walking animations

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -11,17 +11,33 @@ public class PlayerMovement : MonoBehaviour
     // Private class variables.
     private Vector2 Movement;
     private Rigidbody2D RigidBody;
+    private Animator PlayerAnim;
 
     // --------------- Functions --------------- //
 
     private void Awake()
     {
-        RigidBody = GetComponent<Rigidbody2D>();  
+        RigidBody = GetComponent<Rigidbody2D>();
+        PlayerAnim = GetComponent<Animator>();
     }
 
     private void OnMovement(InputValue _Value) // Called when character moves.
     {
         Movement = _Value.Get<Vector2>(); // Assign character movement.
+
+        // Setting the x and y values for the animation.
+        if (Movement.x != 0 || Movement.y != 0)
+        {
+            PlayerAnim.SetFloat("x", Movement.x);
+            PlayerAnim.SetFloat("y", Movement.y);
+
+            PlayerAnim.SetBool("IsWalking", true); // If player is moving play the walk animation.
+        }
+        else
+        {
+            PlayerAnim.SetBool("IsWalking", false);
+        }
+
     }
 
     private void FixedUpdate()
