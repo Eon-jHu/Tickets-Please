50e4655341da33cb6eefe17a8d0dce16df97f18c
Lisa Patricia Kelly <lisa.kelly1@MDS.ac.nz>
2024-06-12 20:38:36 +1200

    Ability to add another NPC
    - Place a 2D sphere collider as trigger only on all NPCs, in addition to the usual 2D box collider.

--- Assets/Scripts/Dialogue/ConversableObject.cs
+++ Assets/Scripts/Dialogue/ConversableObject.cs
@@ -5,21 +5,36 @@ using UnityEngine;
 public class ConversableObject : InteractableObject
 {
     public InitialDialogue m_InitialDialogue;
+    public bool playerIsClose;
 
     protected override void OnInteract()
     {
         if (!HasInteracted)
         {
-            base.OnInteract();
+            if (playerIsClose)
+            {
+                base.OnInteract();
 
-            Debug.Log(m_InitialDialogue.m_Name + " is speaking...");
-            // Start Dialoguing
-            EncounterManager.Instance.StartEncounter(m_InitialDialogue, this, EncounterState.NPCTalking);
+                Debug.Log(m_InitialDialogue.m_Name + " is speaking...");
+                // Start Dialoguing
+                EncounterManager.Instance.StartEncounter(m_InitialDialogue, this, EncounterState.NPCTalking);
+            }
         }
         // If alreading interacting, simulate "CONTINUE" button
         else
         {
             EncounterManager.Instance.ContinueEncounter();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Triggered");
+
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Entered proximity");
+            playerIsClose = true;
         }
     }
 }
